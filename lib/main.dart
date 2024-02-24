0835457a209ba73cb57fc0ca53ea3eecda0b1f4a
Tayba <taybarahmankhan8@gmail.com>
2024-02-23 18:33:55 -0800

    Initial commit.

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:billionaireii/add_money.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  double balance = 0;
+  void addMoney() async {
+    setState(() {
+      balance = balance + 500.0;
+    });
+    // Obtain shared preferences.
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setDouble('balance', balance);
+    print(balance);
+  }
+
+  void loadBalance() async {
+    // Obtain shared preferences.
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      balance = prefs.getDouble('balance')??0;
+    });
+  }
+  @override
+  void initState() {
+    loadBalance();
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Billionaireii'),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(20.0),
+          color: Colors.blueGrey,
+          height: double.infinity,
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                flex: 9,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Bank Balance: '),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Text('$balance'),
+
+                  ],
+                ),
+              ),
+              AddMoneyButton(addMoneyFunction: addMoney,),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
